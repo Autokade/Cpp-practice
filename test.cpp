60e09aef6d68fa40e8a983b560ba1f0d61473353
Autokade <shandilyachinmay@gmail.com>
2024-08-02 00:16:43 +0530

    pattern1

--- test.cpp
+++ test.cpp
@@ -1,5 +1,8 @@
-#include <iostream>
+#include <bits/stdc++.h>  
+using namespace std;
 int main(){
-    std::cout<< "Hello world";
+    int x,y;
+    cin >> x >> y ;
+    cout << "Value of x: "<< x << " and y: " << y;
     return 0;
 }
