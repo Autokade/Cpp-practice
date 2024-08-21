fe9df59180e47676577cee469786390798526e9b
Autokade <shandilyachinmay@gmail.com>
2024-08-21 19:37:14 +0530

    arrays

---
+++ Arrays/CheckingSort.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int isSorted(int n, vector<int> a)
+{
+    for (int i = 1; i < n; i++)
+    {
+        /* code */
+        if (a[i] >> a[i - 1])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+int main()
+{
+}
