37d265113d8e51050878ddc8723cd89c182f18d3
Autokade <shandilyachinmay@gmail.com>
2024-10-08 19:33:03 +0530

    linked list

--- Linked_List/linked_list_menu.cpp
+++ Linked_List/linked_list_menu.cpp
@@ -134,3 +134,75 @@ public:
         cout << "Null\n" ;
     }
 };
+
+int main() {
+    SinglyLinkedList list;
+    int choice, value, pos;
+
+    do {
+        cout << "\nMenu:\n";
+        cout << "1. Insert at beginning\n";
+        cout << "2. Insert at end\n";
+        cout << "3. Insert after a node\n";
+        cout << "5. Delete from beginning\n";
+        cout << "6. Delete from end\n";
+        cout << "7. Delete a specific node\n";
+        cout << "8. Search for a node\n";
+        cout << "9. Display all nodes\n";
+        cout << "10. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter value to insert at beginning: ";
+                cin >> value;
+                list.insertatBeginning(value);
+                break;
+            case 2:
+                cout << "Enter value to insert at end: ";
+                cin >> value;
+                list.insertatEnd(value);
+                break;
+            case 3:
+                int afterValue;
+                cout << "Enter the value of the node after which to insert: ";
+                cin >> afterValue;
+                cout << "Enter value to insert: ";
+                cin >> value;
+                list.insertAfter(afterValue, value);
+                break;
+            case 5:
+                list.deleteFromBeginning();
+                break;
+            case 6:
+                list.deleteFromEnd();
+                break;
+            case 7:
+                cout << "Enter value of the node to delete: ";
+                cin >> value;
+                list.deleteSpecificNode(value);
+                break;
+            case 8:
+                cout << "Enter value to search for: ";
+                cin >> value;
+                pos = list.searchNode(value);
+                if (pos != -1) {
+                    cout << "Node found at position: " << pos << endl;
+                } else {
+                    cout << "Node not found\n";
+                }
+                break;
+            case 9:
+                list.displayList();
+                break;
+            case 10:
+                cout << "Exiting...\n";
+                break;
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice != 10);
+
+    return 0;
+}
