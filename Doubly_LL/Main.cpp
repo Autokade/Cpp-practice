29bc703fbac2df3542883878281096d302b7bceb
Autokade <shandilyachinmay@gmail.com>
2024-10-11 15:12:08 +0530

    dll

---
+++ Doubly_LL/Main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int value) : data(value), next(nullptr), prev(nullptr) {}
+};
+
+class DoublyLinkedList {
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+    void insertAtEnd(int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+
+    void displayForward() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
+    }
+
+    void displayBackward() {
+        Node* temp = tail;
+        while (temp != nullptr) {
+            cout << temp->data << " -> ";
+            temp = temp->prev;
+        }
+        cout << "NULL\n";
+    }
+};
+
+int main() {
+    DoublyLinkedList list;
+
+    list.insertAtEnd(1);
+    list.insertAtEnd(2);
+    list.insertAtEnd(3);
+    list.insertAtEnd(4);
+
+    cout << "List in forward direction: ";
+    list.displayForward();
+
+    cout << "List in backward direction: ";
+    list.displayBackward();
+
+    return 0;
+}
