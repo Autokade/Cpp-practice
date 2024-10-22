8f65eed9bf75771298f78b98cf6e57c6040bdd7d
Autokade <shandilyachinmay@gmail.com>
2024-10-22 09:43:30 +0530

    bst

--- Trees/binarySearchTree/MaximumElement.cpp
+++ Trees/binarySearchTree/MaximumElement.cpp
@@ -1,30 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
+struct Node
+{
     int data;
-    Node* left;
-    Node* right;
-    Node(int val): data(val), left(nullptr), right(nullptr){}
-
-
+    Node *left;
+    Node *right;
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
 };
 
-Node* MaximumElement(Node* root){
-    while(root->right != nullptr){
+Node *MaximumElement(Node *root)
+{
+    while (root->right != nullptr)
+    {
         root = root->right;
     }
     return root;
 }
 
-
-int main() {
-    Node* root = new Node(20);
+int main()
+{
+    Node *root = new Node(20);
     root->left = new Node(10);
     root->right = new Node(30);
     root->left->left = new Node(5);
     root->left->right = new Node(15);
     root->right->right = new Node(40);
 
-Node* max = findMax(root);
+    Node *max = MaximumElement(root);
     cout << "Max: " << max->data << endl;
+}
