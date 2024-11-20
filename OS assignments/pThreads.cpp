727ca6e44d4f1ee37405139d9c1209ef7d1cdbfd
Autokade <shandilyachinmay@gmail.com>
2024-11-21 01:31:53 +0530

    OS

---
+++ OS assignments/pThreads.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#include<pthread.h>
+#include<unistd.h>
+using namespace std;
+
+
+int g = 0;
+
+void* myThreadFun(void* vargp){
+    pid_t myid = getpid();
+
+    static int s = 0;
+
+    ++s;
+    ++g;
+
+cout << "Thread ID: " << myid << ", Static: " << s << ", Global: ";
+
+return nullptr;
+}
+
+int main(){
+    pthread_t tid;
+
+    for(int i= 0; i < 3; i++){
+        pthread_create(&tid, NULL, myThreadFun, NULL);
+        
+    }
+    pthread_exit(NULL);
+    return 0;
+}
