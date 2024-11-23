0717f8d0516c7ffd5a649b35c87e1733d399f4b9
Autokade <shandilyachinmay@gmail.com>
2024-11-24 03:42:31 +0530

    OS

--- OS assignments/Priority_scheduling.cpp
+++ OS assignments/Priority_scheduling.cpp
@@ -1,108 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define totalprocess 5
-
-struct process
-{
-    int at, bt, pr, pno;
-};
-
-process proc[50];
-
-bool comp(process a, process b)
-{
-    if (a.at == b.at)
-    {
-        return a.pr < b.pr;
-    }
-    else
-    {
-        return a.at < b.at;
-    }
-}
-
-void get_wt_time(int wt[])
-{
-    int service[50];
-
-    service[0] = proc[0].at;
-    wt[0] = 0;
-
-    for (int i = 1; i < totalprocess; i++)
-    {
-        service[i] = proc[i - 1].bt + service[i - 1];
-        wt[i] = service[i] - proc[i].at;
-
-        if (wt[i] < 0)
-        {
-            wt[i] = 0;
+// Function to calculate waiting time for all processes
+void findWaitingTime(int processes[], int n, int bt[], int wt[], int priority[]) {
+    // Sorting processes by priority (lower value = higher priority)
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (priority[j] > priority[j + 1]) {
+                swap(priority[j], priority[j + 1]);
+                swap(bt[j], bt[j + 1]);
+                swap(processes[j], processes[j + 1]);
+            }
         }
     }
-}
 
-void get_tat_time(int tat[], int wt[])
-{
-    for (int i = 0; i < totalprocess; i++)
-    {
-        tat[i] = proc[i].bt + wt[i];
+    wt[0] = 0; // Waiting time for the first process is 0
+
+    for (int i = 1; i < n; i++) {
+        wt[i] = bt[i - 1] + wt[i - 1];
     }
 }
 
-void findgc()
-{
-    int wt[50], tat[50];
-
-    double wavg = 0, tavg = 0;
-
-    get_wt_time(wt);
-
-    get_tat_time(tat, wt);
-
-    int stime[50], ctime[50];
-
-    stime[0] = proc[0].at;
-    ctime[0] = stime[0] + tat[0];
-
-    for (int i = 0; i < totalprocess; i++)
-    {
-        stime[i] = ctime[i - 1];
-        ctime[i] = stime[i] + tat[i] - wt[i];
+// Function to calculate turnaround time for all processes
+void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[]) {
+    for (int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
     }
-
-    cout << "Process_no\tStart_time\tComplete_time\tTurn_Around_Time\tWaiting_Time" << endl;
-
-    for (int i = 0; i < totalprocess; i++)
-    {
-        wavg += wt[i];
-        tavg += tat[i];
-
-        cout << proc[i].pno << "\t\t" << stime[i] << "\t\t" << ctime[i] << "\t\t" << tat[i] << "\t\t\t" << wt[i] << endl;
-    }
-
-    cout << "Average waiting time is : ";
-    cout << wavg / (float)totalprocess << endl;
-    cout << "average turnaround time : ";
-    cout << tavg / (float)totalprocess << endl;
 }
 
-int main()
-{
-    int arrivaltime[] = {1, 2, 3, 4, 5};
-    int bursttime[] = {3, 5, 1, 7, 4};
-    int priority[] = {3, 4, 1, 7, 8};
+// Function to calculate average time
+void findAvgTime(int processes[], int n, int bt[], int priority[]) {
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
 
-    for (int i = 0; i < totalprocess; i++)
-    {
-        proc[i].at = arrivaltime[i];
-        proc[i].bt = bursttime[i];
-        proc[i].pr = priority[i];
-        proc[i].pno = i+1;
+    // Calculate waiting time for all processes
+    findWaitingTime(processes, n, bt, wt, priority);
+
+    // Calculate turnaround time for all processes
+    findTurnAroundTime(processes, n, bt, wt, tat);
+
+    // Display processes along with details
+    cout << "Processes  " << "Burst Time  " << "Priority  "
+         << "Waiting Time  " << "Turnaround Time\n";
+
+    for (int i = 0; i < n; i++) {
+        total_wt += wt[i];
+        total_tat += tat[i];
+        cout << " " << processes[i] << "\t\t" << bt[i] << "\t    " << priority[i]
+             << "\t\t" << wt[i] << "\t\t" << tat[i] << "\n";
     }
 
-    sort(proc, proc+totalprocess, comp);
+    // Display average waiting and turnaround times
+    cout << "\nAverage waiting time = " << (float)total_wt / n;
+    cout << "\nAverage turnaround time = " << (float)total_tat / n << "\n";
+}
 
-    findgc();
+// Main function
+int main() {
+    int processes[] = {1, 2, 3}; // Process IDs
+    int n = sizeof(processes) / sizeof(processes[0]);
+
+    int burst_time[] = {10, 5, 8}; // Burst times for each process
+    int priority[] = {3, 1, 2};    // Priorities for each process (lower = higher priority)
+
+    findAvgTime(processes, n, burst_time, priority);
+
     return 0;
-    
 }
