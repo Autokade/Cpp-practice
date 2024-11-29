4cc579c460d0dd96e3a697076f91436cac12913f
Autokade <shandilyachinmay@gmail.com>
2024-11-29 07:59:12 +0530

    MATLAB

---
+++ MATLAB/trap&sim.m
@@ -0,0 +1,23 @@
+clc;
+clear all;
+f = @(x) (exp(-x*x)*cos(x));
+a = input('Enter the lower limit: ');
+b = input('Enter the upper limit: ');
+n = input('Enter number of intervals: ');
+h = ((b-a)/n);
+sum = 0;
+for i = 1:n-1
+    sum = sum + 2* f(a+i*h);
+end
+trap = (h/2)*(f(a)+f(b)+sum);
+disp(trap);
+
+sum1 = 0;
+for i = 1:n-1
+   if rem(i,2)==0
+       sum1=sum1+2*f(a+i*h)
+   else
+       sum1=sum1+4*f(a+i*h)
+end
+simpson = (h/3)*(f(a)+sum1+f(b));
+disp(simpson);
