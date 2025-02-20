074531351ad216bc94beaa930845701510b45e35
Autokade <shandilyachinmay@gmail.com>
2025-02-20 14:48:44 +0530

    R

---
+++ R/practice.R
@@ -0,0 +1,10 @@
+sum <- 10
+print(sum)
+sum1 <- sum + 2
+print(sum1)
+
+num = c(5,10,15,20,25,30)
+print("original number is : ")
+print(num)
+print(paste("Maximum value of said vector: ", max(num)))
+print(paste("Minimum value of said vector: ", min(num)))
